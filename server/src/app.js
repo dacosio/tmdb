d644feb15e0130dc1d2d561dd842d5f8ede64ce4
Don Cosio <dacosio@gmail.com>
2023-11-14 15:40:38 -0800

    refactor

--- server/src/app.js
+++ server/src/app.js
@@ -6,38 +6,26 @@ const port = process.env.PORT || 3000;
 const apiURL = "https://api.themoviedb.org/3";
 const apiKey = "&api_key=2b8ae5af818b4145f34d48e0264179f9&page=1";
 const accessToken =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjhhZTVhZjgxOGI0MTQ1ZjM0ZDQ4ZTAyNjQxNzlmOSIsInN1YiI6IjYyYjhmMTEwZTFmYWVkMGI4MTE3NzFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qoEG-px9xzL2SYIl3K1g8F1H4VUsjGFBpXsSRSv2-fk"; // Replace with your actual access token
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjhhZTVhZjgxOGI0MTQ1ZjM0ZDQ4ZTAyNjQxNzlmOSIsInN1YiI6IjYyYjhmMTEwZTFmYWVkMGI4MTE3NzFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qoEG-px9xzL2SYIl3K1g8F1H4VUsjGFBpXsSRSv2-fk";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Initial fetch for popular movies
-app.get("/", async (req, res) => {
-  try {
-    const data = await fetchMovies("/discover/movie?sort_by=popularity.desc");
-    res.send(data.results);
-  } catch (error) {
-    console.error("Error fetching popular movies:", error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
-});
+// Endpoints
+app.get("/", getPopularMovies);
+app.get("/search", searchMovies);
+app.post("/favorite", manageFavorites);
+app.get("/favorite", getFavoriteMovies);
 
-// Search function
-app.get("/search", async (req, res) => {
-  try {
-    const { movieName } = req.query;
-    const apiSearch = `/search/movie?query=${movieName}&include_adult=false&language=en-US`;
-    const data = await fetchMovies(apiSearch);
-    res.send(data.results);
-  } catch (error) {
-    console.error("Error searching movies:", error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
-});
+// Function to fetch movies from the Movie Database API
+async function fetchMovies(endpoint) {
+  const response = await fetch(apiURL + endpoint + apiKey);
+  return response.json();
+}
 
-// POST endpoint for adding/removing movies to/from favorites
-app.post("/favorite", async (req, res) => {
+// Function to handle the "/favorite" endpoint
+async function manageFavorites(req, res) {
   try {
     const { mediaId, isFavorite } = req.body;
     const url = "https://api.themoviedb.org/3/account/12901691/favorite";
@@ -62,10 +50,34 @@ app.post("/favorite", async (req, res) => {
     console.error("Error managing favorites:", error);
     res.status(500).send({ error: "Internal Server Error" });
   }
-});
+}
 
-// Fetch favorite movies
-app.get("/favorite", async (req, res) => {
+// Function to handle the "/" endpoint
+async function getPopularMovies(req, res) {
+  try {
+    const data = await fetchMovies("/discover/movie?sort_by=popularity.desc");
+    res.send(data.results);
+  } catch (error) {
+    console.error("Error fetching popular movies:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+}
+
+// Function to handle the "/search" endpoint
+async function searchMovies(req, res) {
+  try {
+    const { movieName } = req.query;
+    const apiSearch = `/search/movie?query=${movieName}&include_adult=false&language=en-US`;
+    const data = await fetchMovies(apiSearch);
+    res.send(data.results);
+  } catch (error) {
+    console.error("Error searching movies:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+}
+
+// Function to handle the "/favorite" endpoint
+async function getFavoriteMovies(req, res) {
   try {
     const data = await fetchFavoriteMovies();
     res.send(data.results);
@@ -73,16 +85,6 @@ app.get("/favorite", async (req, res) => {
     console.error("Error fetching favorite movies:", error);
     res.status(500).send({ error: "Internal Server Error" });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is up and running at port ${port}`);
-});
-
-// Function to fetch movies from the Movie Database API
-async function fetchMovies(endpoint) {
-  const response = await fetch(apiURL + endpoint + apiKey);
-  return response.json();
 }
 
 // Function to fetch favorite movies
@@ -100,3 +102,7 @@ async function fetchFavoriteMovies() {
   const response = await fetch(url, options);
   return response.json();
 }
+
+app.listen(port, () => {
+  console.log(`Server is up and running at port ${port}`);
+});
